881e36f75c294983327fd51189805f24c3ecd119
wikkutek <zszczerbiak.neuca@gmail.com>
2026-02-11 16:44:43 +0100

    dodanie bibliotek i pliku

---
+++ analiza.py
@@ -0,0 +1,5 @@
+# PLIK Z WYŚWIETLANIEM
+# Import bibliotek
+import pandas as pd
+import numpy as np
+import openpyxl as xl
--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+# PLIK Z WYŚWIETLANIEM
+# Import bibliotek
 import streamlit as st
+import pandas as pd
+import numpy as np
+import openpyxl as xl
+
+
+
+
+
+
 
 st.title("Aplikacja z danymi studentów 2026")
