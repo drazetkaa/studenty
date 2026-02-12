c732bffdf52c90d84c7daf0bff2d2249475305b4
wikkutek <zszczerbiak.neuca@gmail.com>
2026-02-12 17:01:45 +0100

    wczytanie pliku

--- analiza.py
+++ analiza.py
@@ -1,5 +1,14 @@
-# PLIK Z WYŚWIETLANIEM
+# PLIK Z ANALIZĄ DANYCH
+
 # Import bibliotek
 import pandas as pd
 import numpy as np
 import openpyxl as xl
+
+# Wczytanie pliku z danymi
+plik = 'PZ 2025 dane.xlsx'
+df = pd.read_excel(plik)
+
+# Obejrzenie zawartości pliku
+print(df.info())
+
